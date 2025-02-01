b4a7d3482220f3dce4fb96edc8202c7198caf5ef
Antonio Queb <antonioqueb@alphaqueb.online>
2025-02-01 00:31:36 -0600

    Up

--- models/res_partner.py
+++ models/res_partner.py
@@ -6,11 +6,18 @@ class ResPartner(models.Model):
 
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
-        """Filtra por 'company_registry' solo si el contexto 'from_sale_order' está presente."""
+        """
+        Aplica búsqueda exacta por 'company_registry' SOLO si el contexto
+        'from_sale_order' está presente (es decir, cuando estamos en Ventas).
+        En otros módulos, la búsqueda se realiza de forma nativa.
+        """
+        # Solo filtra si estamos en contexto de Ventas Y el usuario teclea algo
         if self.env.context.get('from_sale_order') and name:
             args = args or []
-            args += [('company_registry', '=', name)]
-            # Llama al método base con name='' para ignorar búsqueda por nombre
-            return super().name_search(name='', args=args, operator=operator, limit=limit)
+            # Búsqueda EXACTA por company_registry
+            args.append(('company_registry', '=', name))
+            # Pasamos name='' para ignorar coincidencia por nombre y forzar la búsqueda anterior
+            return super(ResPartner, self).name_search(name='', args=args, operator=operator, limit=limit)
         else:
-            return super().name_search(name, args, operator, limit)
+            # En cualquier otro módulo (Compras, Contabilidad, etc.), búsqueda nativa
+            return super(ResPartner, self).name_search(name, args, operator, limit)
