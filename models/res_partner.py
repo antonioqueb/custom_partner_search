b158367aa6ce82f0651f0c60cfc3fa5b1220ca69
Antonio Queb <antonioqueb@alphaqueb.online>
2025-02-04 09:58:53 -0600

    Up

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,4 +1,3 @@
-# models/res_partner.py
 from odoo import models, api
 
 class ResPartner(models.Model):
@@ -7,15 +6,15 @@ class ResPartner(models.Model):
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
         """
-        Aplica búsqueda exacta por 'company_registry' SOLO si el contexto
+        Aplica búsqueda parcial ('ilike') por 'company_registry' SOLO si el contexto
         'from_sale_order' está presente (es decir, cuando estamos en Ventas).
         En otros módulos, la búsqueda se realiza de forma nativa.
         """
         # Solo filtra si estamos en contexto de Ventas Y el usuario teclea algo
         if self.env.context.get('from_sale_order') and name:
             args = args or []
-            # Búsqueda EXACTA por company_registry
-            args.append(('company_registry', '=', name))
+            # Búsqueda PARCIAL por company_registry
+            args.append(('company_registry', 'ilike', name))
             # Pasamos name='' para ignorar coincidencia por nombre y forzar la búsqueda anterior
             return super(ResPartner, self).name_search(name='', args=args, operator=operator, limit=limit)
         else:
